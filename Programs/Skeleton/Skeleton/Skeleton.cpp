c1bb4fd30ae0cf4f63fc0e68e36d8c704bf9edac
unknown <bazsi4692@gmail.com>
2020-10-31 20:21:28 +0100

    Load Texture

---
+++ Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -0,0 +1,112 @@
+#include "framework.h"
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+
+GPUProgram gpuProgram; // vertex and fragment shaders
+unsigned int vao;	   // virtual world on the GPU
+Texture3D texture;
+
+// Initialization, create an OpenGL context
+void onInitialization() {
+	ShaderProgramSource source = parserShader("./vertex.vert", "./fragment.frag");
+	glViewport(0, 0, windowWidth, windowHeight);
+	// create program for the GPU
+	gpuProgram.create(&(source.VertexSource[0]), &(source.FragmentSource[0]), "outColor");
+
+	texture = Texture3D("./res/stagbeetle.dat");
+
+	glGenVertexArrays(1, &vao);	// get 1 vao id
+	glBindVertexArray(vao);		// make it active
+
+	unsigned int vbo;		// vertex buffer object
+	glGenBuffers(1, &vbo);	// Generate 1 buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	// Geometry 
+	float cVtx[] = { -1,-1, 1,-1, 1,1, -1, 1 };
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cVtx),
+		cVtx, GL_STATIC_DRAW);
+
+
+	glEnableVertexAttribArray(0);  // AttribArray 0
+	glVertexAttribPointer(0,       // vbo -> AttribArray 0
+		2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
+		0, NULL); 		     // stride, offset: tightly packed
+
+
+}
+
+// Window has become invalid: Redraw
+void onDisplay() {
+	glClearColor(0, 0, 0, 0);     // background color
+	glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
+
+	// Set color to (0, 1, 0) = green
+	int location = glGetUniformLocation(gpuProgram.getId(), "color");
+	glUniform3f(location, .1f, 0.0f, .1f); // 3 floats
+
+	float MVPtransf[4][4] = { 1, 0, 0, 0,    // MVP matrix, 
+							  0, 1, 0, 0,    // row-major!
+							  0, 0, 1, 0,
+							  0, 0, 0, 1 };
+
+	location = glGetUniformLocation(gpuProgram.getId(), "MVP");	// Get the GPU location of uniform variable MVP
+	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);	// Load a 4x4 row-major float matrix to the specified location
+
+
+	/*glActiveTexture(GL_TEXTURE0);
+	int texture_location = glGetUniformLocation(gpuProgram.getId(), "vol");
+	glUniform1i(texture_location, 0);
+	glBindTexture(GL_TEXTURE_3D, texture.textureId);*/
+	gpuProgram.setUniform(texture, std::string("vol"));
+
+	glBindVertexArray(vao);  // Draw call
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+	glutSwapBuffers(); // exchange buffers for double buffering
+}
+
+// Key of ASCII code pressed
+void onKeyboard(unsigned char key, int pX, int pY) {
+	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+}
+
+// Key of ASCII code released
+void onKeyboardUp(unsigned char key, int pX, int pY) {
+}
+
+// Move mouse with key pressed
+void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
+	// Convert to normalized device space
+	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
+	float cY = 1.0f - 2.0f * pY / windowHeight;
+}
+
+// Mouse click event
+void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
+	// Convert to normalized device space
+	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
+	float cY = 1.0f - 2.0f * pY / windowHeight;
+
+	char* buttonStat;
+	switch (state) {
+	case GLUT_DOWN: buttonStat = "pressed"; break;
+	case GLUT_UP:   buttonStat = "released"; break;
+	}
+
+	switch (button) {
+	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
+	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
+	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
+	}
+}
+
+// Idle event indicating that some time elapsed: do animation here
+void onIdle() {
+	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+}
